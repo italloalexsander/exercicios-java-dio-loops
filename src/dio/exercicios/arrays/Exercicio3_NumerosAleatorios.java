948b69c18643d83c237b829275d5ef9e5fe481a3
Itallo <italloalexsander@gmail.com>
2022-04-29 20:16:00 -0300

    Exercicios 3 feito do meu jeito

---
+++ src/dio/exercicios/arrays/Exercicio3_NumerosAleatorios.java
@@ -0,0 +1,41 @@
+package dio.exercicios.arrays;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+
+import java.util.Scanner;
+
+public class Exercicio3_NumerosAleatorios {
+    public static void main(String[] args) {
+        int[] vetor = new int[20];
+        int vetorTamanho = vetor.length;
+        int numero;
+        Scanner input = new Scanner(System.in);
+
+        for(int i = 0; i < vetorTamanho; i++){
+            while (true) {
+                System.out.println("Insira um numero válido(0-100): ");
+                numero = input.nextInt();
+                if (numero >= 0 && numero <= 100) {
+                    vetor[i] = numero;
+                    System.out.println("Número " + numero + " inserido na posição " + i + " do vetor!");
+                    break;
+                }
+            }
+        }
+
+        System.out.print("Números: [");
+        for(int j: vetor){
+            System.out.print(j + " ");
+        }
+        System.out.println("]\n");
+
+        System.out.print("Sucessores: [");
+        for(int j: vetor){
+            System.out.print((j + 1) + " ");
+        }
+        System.out.println("]");
+    }
+}
